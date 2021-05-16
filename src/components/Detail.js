40f28de897328215d672dabeda8359aced7f0513
jay75chauhan <chauhan75jayk@gmail.com>
2021-05-16 18:06:34 +0530

    initial commit

---
+++ src/components/Detail.js
@@ -0,0 +1,143 @@
+import styled from "styled-components";
+
+function Detail() {
+  return (
+    <Container>
+      <Background>
+        <img src="https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1_5x/sources/r1/cms/prod/2539/962539-h" />
+      </Background>
+      <ImageTitle>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
+
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>Trailer</span>
+        </TrailerButton>
+
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupWatchButton>
+      </Controls>
+
+      <SubTitle>2018 . 7m .Family, Fantasy, Kid, Animation</SubTitle>
+
+      <Description>
+        The Big Bull is a 2021 Indian Hindi-language crime drama film directed
+        and co-written by Kookie Gulati and produced by Ajay Devgn, Anand
+        Pandit, Vikrant Sharma and Kumar Mangat Pathak.
+      </Description>
+    </Container>
+  );
+}
+
+export default Detail;
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 5px;
+  height: 56px;
+  padding: 0px 24px;
+  margin-right: 22px;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  background: rgb(249, 249, 249);
+  border: none;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgb(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+
+const AddButton = styled.button`
+  width: 44px;
+  height: 44px;
+  margin-right: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.6);
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 26px;
+  color: rgb(249, 249, 249);
+`;
---
+++ src/components/Login.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <Container>
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg" />
+        <SingUp>GET ALL THERE</SingUp>
+        <Description>
+          Disney+ is the home for your favorite movies and TV shows from Disney,
+          Pixar, Marvel, Star Wars, and National Geographic. Sign up for Disney+
+          and start ...
+        </Description>
+
+        <CTALogoTwo src="/images/cta-logo-two.png" />
+      </CTA>
+    </Container>
+  );
+}
+
+export default Login;
+
+const Container = styled.div`
+  position: relative;
+
+  min-height: calc(100vh - 70px);
+  display: flex;
+  align-items: top;
+  justify-content: center;
+
+  &:before {
+    background-image: url("/images/login-background.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    content: "";
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0.7;
+    z-index: -1;
+  }
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+  padding: 80px 40px;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+  align-items: center;
+`;
+
+const CTALogoOne = styled.img``;
+
+const SingUp = styled.a`
+  width: 100%;
+  background-color: #0063e5;
+  font-weight: bold;
+  padding: 17px 0;
+  color: #f9f9f9;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 18px;
+  cursor: pointer;
+  transition: all 250ms;
+  letter-spacing: 1.5px;
+  margin-top: 8px;
+  margin-top: 12px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  font-size: 11px;
+  letter-spacing: 1.5px;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+const CTALogoTwo = styled.img`
+  width: 90%;
+`;
